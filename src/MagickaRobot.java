55f5e8d7f9a5a66f564701920ba87ce9f0e892cb
Erwan NORMAND <normand.erwan@gmail.com>
2016-01-23 16:57:22 -0500

    Create a magicka_path.txt file in the root, containing the path to the Magicka.exe

--- src/MagickaRobot.java
+++ src/MagickaRobot.java
@@ -1,11 +1,32 @@
 import java.awt.AWTException;
 import java.awt.Robot;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MagickaRobot {
 	
 	public MagickaRobot() throws AWTException, IOException {
-		Runtime.getRuntime().exec("C:/Program Files (x86)/Steam/steamapps/common/Magicka/Magicka.exe");
+		String magickaPathError = "Magicka.exe was not found ! Please copy the path to the Magicka.exe program in the file 'magicka_path.txt'";
+		
+		List<String> lines = new ArrayList<String>();
+		try {
+			lines = Files.readAllLines(Paths.get("magicka_path.txt"));
+		} catch (IOException e1) {
+			throw new IOException(magickaPathError);
+		}
+		
+		if (lines.isEmpty()) {
+			throw new IOException(magickaPathError);
+		} else {
+			try {
+				Runtime.getRuntime().exec(lines.get(0));
+			} catch (IOException e) {
+				throw new IOException(magickaPathError);
+			}
+		}
 		
 		this.robot = new Robot();
 	}
