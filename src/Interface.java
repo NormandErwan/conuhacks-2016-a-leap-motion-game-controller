995ed88a093c41b711bff7c1f7e11a54e0b0ac40
G4suleg <o.gregoire07@gmail.com>
2016-01-24 02:59:08 -0500

    les listener pour les gestes fonctionne!

--- src/Interface.java
+++ src/Interface.java
@@ -15,53 +15,54 @@ public class Interface extends JFrame {
     private String pathGame;
     private char[] tabKey;
     private int nbrGestures;
-    private JTextField varGame;
+    private JTextField varGame[];
     private JTextField JTnbrGesture;
     private PoseManager poseManager;
-	
+    private JButton[] button;
+
     Interface(PoseManager poseManager) {
-		this.poseManager = poseManager;
-		top();
-		
-		bot();
-	
-		initUI();
+	this.poseManager = poseManager;
+	top();
+
+	bot();
+
+	initUI();
     }
     /**
      * Initializes the main properties of the game window
      */
     public void initUI() {
-		frame.setTitle("Réglage du LeapMotion pour le jeu");
-		frame.setSize(1000, 600);
-		frame.setLocationRelativeTo(null);
-		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		frame.setVisible(true);
+	frame.setTitle("Réglage du LeapMotion pour le jeu");
+	frame.setSize(1000, 600);
+	frame.setLocationRelativeTo(null);
+	frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	frame.setVisible(true);
     }
     public void top(){
 	JPanel topPanel = new JPanel();
 	frame.add(topPanel, BorderLayout.NORTH);
 	JTnbrGesture = new JTextField("Nombre Gesture");
 	topPanel.add(JTnbrGesture);
-	
+
 	JTnbrGesture.addActionListener(new listenerNbrGesture());
 
     }
-  public class listenerNbrGesture implements ActionListener {
+    public class listenerNbrGesture implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-	  
-	    
 	    nbrGestures = JTnbrGesture.getText().charAt(0)-48; //ascii
 	    System.out.println(nbrGestures);
 	    mid();
 	    //tabKey[0]= varGame.getText().charAt(0);
-	    
+
 	}
-  }
+    }
     public void mid(){
-	char[] alphabet = new char[]{'q','w','e','r','a','s','d','f'};
+	button = new JButton[nbrGestures];
 	JFrame frameGesture = new JFrame("Gesture");
+
+	varGame = new JTextField[nbrGestures];
 	frameGesture.setSize(1000, 600);
 	frameGesture.setLocationRelativeTo(null);
 	frameGesture.setVisible(true);
@@ -70,40 +71,60 @@ public class Interface extends JFrame {
 	GridLayout midGridLayout = new GridLayout(nbrGestures,2);
 	midPanel.setLayout(midGridLayout);
 	midPanel.setPreferredSize(new Dimension(100, 50));
-	for(int i=0; i<nbrGestures; i++){
-	    	varGame = new JTextField(alphabet[i]);
-		JButton button = new JButton("Gesture");
-		midPanel.add(varGame);
-		midPanel.add(button);
-		varGame.addActionListener(new listenerChoiceVarGame());
+	Integer i;
+	for(i=0; i<nbrGestures; i++){
+	    varGame[i] = new JTextField();
+	    button[i] = new JButton("Gesture "+ i.toString());
+	    midPanel.add(varGame[i]);
+	    midPanel.add(button[i]);
+	    varGame[i].addActionListener(new listenerChoiceVarGame());
+	    button[i].addActionListener(new listenerChoiceGesture());
 	}
-	
-	
-	//varGame.addActionListener(new listenerChoiceVarGame());
 
     }
     public class listenerChoiceVarGame implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-	  
-	    System.out.println(varGame.getText().charAt(0));
+	    for(int g =0 ; g<nbrGestures;g++)
+	    {
+	    if(e.getSource() == varGame[g])
+		{ 
+		System.out.println(g);
+		System.out.println(varGame[g].getText().charAt(0));
+		}
+	    }
+	    //System.out.println(varGame.getText().charAt(0));
 	    //tabKey[0]= varGame.getText().charAt(0);
 	    
 	}
-	
+
     }
-    
+    public class listenerChoiceGesture implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+	    for(int g =0 ; g<nbrGestures;g++)
+	    {
+	    if(e.getSource() == button[g])
+		{ 
+		System.out.println(g);
+		}
+	    }
+	}
+
+    }
+
     public void bot(){
-		JPanel botPanel = new JPanel();
-		JButton jouer = new JButton("Jouer");
-		JButton choixJeu =new JButton("Choix jeu");
-		frame.add(mainPanel);
-		mainPanel.add(botPanel, BorderLayout.SOUTH);
-		botPanel.add(choixJeu);
-		botPanel.add(jouer);
-	
-		choixJeu.addActionListener(new listenerChoiceOfTheGame());
+	JPanel botPanel = new JPanel();
+	JButton jouer = new JButton("Jouer");
+	JButton choixJeu =new JButton("Choix jeu");
+	frame.add(mainPanel);
+	mainPanel.add(botPanel, BorderLayout.SOUTH);
+	botPanel.add(choixJeu);
+	botPanel.add(jouer);
+
+	choixJeu.addActionListener(new listenerChoiceOfTheGame());
     }
 
     public class listenerChoiceOfTheGame implements ActionListener {
@@ -112,16 +133,16 @@ public class Interface extends JFrame {
 	public void actionPerformed(ActionEvent e) {
 	    JFrame fcheminJeu = new JFrame();
 	    try {
-	    JFileChooser chooser = new JFileChooser();
-	    chooser.showOpenDialog(null);
-	    File file = chooser.getSelectedFile();
+		JFileChooser chooser = new JFileChooser();
+		chooser.showOpenDialog(null);
+		File file = chooser.getSelectedFile();
 		pathGame = file.getCanonicalPath();
 
 	    } catch (IOException e1) {
 		// TODO Auto-generated catch block
 		e1.printStackTrace();
 	    }
-	    
+
 
 	}
 
